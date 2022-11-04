a3b5d5c8ff5551aa505fa1ff6e8329f186d4c589
Hans Henrik Gabelgaard <hhg@gabelgaard.org>
2022-11-04 09:27:39 +0100

    Fix wrong import

--- membership_extension/models/account_invoice.py
+++ membership_extension/models/account_invoice.py
@@ -4,7 +4,6 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo import api, models
-from future.backports.xmlrpc.client import multi
 
 
 class AccountInvoice(models.Model):
